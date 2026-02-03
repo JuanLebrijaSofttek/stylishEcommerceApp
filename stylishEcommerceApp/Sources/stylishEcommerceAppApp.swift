5274f055406f1b4df4da3cfa8b14006d320301c0
JuanLebrijaSofttek <juan.lebrija@softtek.com>
2026-02-03 15:27:38 -0600

    Add project files from local folder

---
+++ stylishEcommerceApp/Sources/stylishEcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  stylishEcommerceAppApp.swift
+//  Stylish ECommerce App
+//
+//  Created by Frida on 01/20/26.
+//
+
+import SwiftUI
+
+@main
+struct stylishEcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
